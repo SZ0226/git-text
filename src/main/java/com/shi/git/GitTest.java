d99d1d5310009247efb24393d11d54204aaa083c
testshi <1321358591@qq.com>
2022-03-20 19:18:20 +0800

    hot-fix commit2

--- src/main/java/com/shi/git/GitTest.java
+++ src/main/java/com/shi/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,Word");
         System.out.println("hello,Word");
         System.out.println("hello,Word,hot-fix");
+        System.out.println("hot-fix,merge");
     }
 }
