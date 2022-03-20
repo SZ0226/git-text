ee9ab560335f68a84caaaa0b6b625d4d08c603b6
testshi <1321358591@qq.com>
2022-03-20 19:15:17 +0800

    second commit

--- src/main/java/com/shi/git/GitTest.java
+++ src/main/java/com/shi/git/GitTest.java
@@ -3,5 +3,6 @@ package com.shi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Word");
+        System.out.println("hello,Word");
     }
 }
